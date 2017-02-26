f785a5f0dbade77b874c999f829a0dfd1c788973
rombromb <romaromb@gmail.com>
2017-02-26 20:29:31 +0400

    Update svetofor.py

--- svetofor.py
+++ svetofor.py
@@ -2,7 +2,7 @@ from datetime import datetime
 now = datetime.now().minute
 print now
     color = n % 5
-    if color < 3
+    if 0 < color < 3
         print('green')
     else:
         print('red')
